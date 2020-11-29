d3363ab3b8dac921a4e98e016d32928f948225ca
luo-Y400 <sf@gmail.com>
2020-11-29 21:32:29 +0800

    feat:  add onclick event

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './importIcons';
+import './icon.scss';
 
 interface IconProps {
   name: string;
+  onClick: () => void;
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props) => {
   return (
-    <span>
-      <svg>
-        <use xlinkHref={`#${props.name}`} />
-      </svg>
-    </span>
+    <svg className="luo-icon" onClick={props.onClick}>
+      <use xlinkHref={`#${props.name}`} />
+    </svg>
   );
 };
 
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,3 +1,14 @@
-const div = document.createElement('div');
-div.innerHTML = 'hi';
-document.body.appendChild(div);
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Icon from './icon';
+
+const fn = () => {
+  console.log('fn');
+};
+
+ReactDOM.render(
+  <div>
+    <Icon name="qq" onClick={fn} />
+  </div>,
+  document.querySelector('#root')
+);

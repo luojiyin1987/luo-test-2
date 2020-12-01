c38f5ace80c14f1c38dcd2ea385a7eef5a28c8f7
luo-Y400 <sf@gmail.com>
2020-12-01 11:27:56 +0800

    refactor: example.tsx  as entry

---
+++ example.tsx
@@ -0,0 +1,10 @@
+import Icon from './lib/icon/icon';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+ReactDOM.render(
+  <div>
+    <Icon name="alipay" />
+  </div>,
+  document.querySelector('#root')
+);
